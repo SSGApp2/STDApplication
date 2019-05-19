5ded7c5f417e95054cec9a17a734ec1f8b7d8749
thanakorn <thanakorn.noy@gmail.com>
2019-05-19 18:05:11 +0700

    Machine insert update delete

--- src/main/java/com/soft/app/controller/SettingController.java
+++ src/main/java/com/soft/app/controller/SettingController.java
@@ -1,11 +1,13 @@
 package com.soft.app.controller;
 
+import com.soft.app.repository.custom.vcc.iot.IotDeviceRepositoryCustom;
 import com.soft.app.repository.custom.vcc.iot.IotMachineRepositoryCustom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/settings")
@@ -13,9 +15,20 @@ public class SettingController {
     @Autowired
     private IotMachineRepositoryCustom iotMachineRepositoryCustom;
 
+    @Autowired
+    IotDeviceRepositoryCustom iotDeviceRepositoryCustom;
+
     @GetMapping("warning")
     private String settingRate(ModelMap model) {
         model.addAttribute("iotMachine", iotMachineRepositoryCustom.findByOuth());
         return "settings/warning";
+    }
+
+    @RequestMapping(value = {"/machine"}, method = RequestMethod.GET)
+    public String machineSetting(ModelMap model) {
+        model.addAttribute("iotMachine",iotMachineRepositoryCustom.findByOuth());
+        model.addAttribute("iotDevice",iotDeviceRepositoryCustom.findByOuth());
+        //FIRST PAGE
+        return "iotmachine/machineSetting";
     }
 }
